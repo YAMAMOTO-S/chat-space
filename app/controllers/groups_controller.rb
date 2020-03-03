c67b402e44d5f6d6ef2254fbe41e2edcb54b87fd
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-03-03 18:50:13 +0900

    create group update function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,6 @@ class GroupsController < ApplicationController
     @group = Group.new
     @group.users << current_user
   end
-
   def create
     @group = Group.new(group_params)
     if @group.save
@@ -13,6 +12,20 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+
+  
+  def edit
+    @group = Group.find(params[:id])
+  end
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: "グループを更新しました"
+    else
+      render :edit
+    end
+  end
+  
 
   private
   def group_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root "messages#index"
 
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
