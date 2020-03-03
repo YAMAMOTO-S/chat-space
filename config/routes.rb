33420e3c439769308dd564f0c9ef3ce5a4d56225
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-03-03 19:11:36 +0900

    change the layout of group/new, edit file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "groups#index"
 
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
