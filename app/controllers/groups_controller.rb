bd74980f81d3c9de0e33123cbd07cf48413119e2
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-03-03 19:42:01 +0900

    changed to be able to see current_user groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,8 @@
 class GroupsController < ApplicationController
 
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root "groups#index"
 
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
