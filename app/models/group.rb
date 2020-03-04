41f31057998760e7094a75cf5ed800c970df8cdb
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-03-04 17:58:50 +0900

    be able to  see member_name, group_name and last_messages

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,17 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if(last_message = messages.last).present?
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      "まだメッセージはありません。"
+    end
+  end
+  
 end
