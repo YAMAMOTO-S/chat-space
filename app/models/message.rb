1a1ba209665b1896f493ee24075e1e1237edcde0
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-03-04 14:46:31 +0900

    made  a create function

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates :content, presence: true, unless: :iamge?
+  validates :content, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
 end
